cc9a459fab02df0b04c1a1fe875600f5299e1706
Brian Sahota <bpsahota@gmail.com>
2021-07-07 10:52:42 -0400

    getting there

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://peadars-woodshop.netlify.app', 'http://localhost:3000' # when you deploy make sure you change this to forntend domain name eg. origins 'briansahotahehe.com'
+    origins '*'
+    # 'https://peadars-woodshop.netlify.app', 'http://localhost:3000' when you deploy make sure you change this to forntend domain name eg. origins 'briansahotahehe.com'
 
     resource '*',
       headers: :any,
