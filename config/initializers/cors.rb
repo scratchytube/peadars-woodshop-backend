ef0738d096ecc884857350aa45d69ddb8a3a33fb
Brian Sahota <bpsahota@gmail.com>
2021-07-07 11:57:19 -0400

    figured out how to update ruby

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins {
+    origins{
       ['https://peadars-woodshop.netlify.app', 'http://localhost:3000']
     }
 
